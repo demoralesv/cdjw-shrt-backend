cfdb9074165932d931dae35f865cf427d0bc15f0
sachihi <sachikola909@gmail.com>
2026-03-01 18:32:42 -0600

    mongodb new document

--- src/models/Url.js
+++ src/models/Url.js
@@ -5,7 +5,10 @@ const UrlSchema = new mongoose.Schema(
     originalUrl: { type: String, required: true },
     code: { type: String, required: true, unique: true, index: true },
     shortUrl: { type: String, required: true },
-    baseUrl: { type: String, required: true }
+    baseUrl: { type: String, required: true },
+    totalAccesses: { type: BigInt, required: false},
+    countries: [{name: {type: String, required: false}}],
+    dailyFrequency: {type: BigInt, required: false}
   },
   { timestamps: { createdAt: "createdAt", updatedAt: "updatedAt" } }
 );
